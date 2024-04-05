5d0d195c7672ad20308b2585410047b9fd755f92
guneyeroglu <benguneyeroglu@gmail.com>
2024-04-05 20:12:03 +0300

    #create: models

---
+++ models/models.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/guneyeroglu/wander-wheels-be/database"
+)
+
+type Model struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Brand Brand  `json:"brand"`
+}
+
+func GetAllModels(c *fiber.Ctx) error {
+	db := database.ConnectDb()
+	defer db.Close()
+
+	var models []Model
+
+	rows, err := db.Query(`
+		SELECT 
+			M.id AS model_id, 
+			M.name AS model_name,
+			M.brand_id AS brand_id,
+			B.name AS brand_name
+		FROM models AS M
+		JOIN brands AS B ON b.id = M.brand_id
+	`)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	for rows.Next() {
+		var model Model
+		var brand Brand
+
+		err := rows.Scan(&model.Id, &model.Name, &brand.Id, &brand.Name)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+
+		model.Brand = brand
+		models = append(models, model)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(models)
+}

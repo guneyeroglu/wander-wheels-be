2e07a7d7e1e53a6f29954b542d885880cafbd34e
guneyeroglu <benguneyeroglu@gmail.com>
2024-04-05 19:51:47 +0300

    #update: added route

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/guneyeroglu/wander-wheels-be/middleware"
+	"github.com/guneyeroglu/wander-wheels-be/models"
 )
-
-func helloWorld(c *fiber.Ctx) error {
-	return c.SendString("Hello, world!")
-}
 
 func main() {
 	app := fiber.New()
+	app.Use(middleware.Language)
+	api := app.Group("/api")
 
-	app.Get("/api", helloWorld)
+	api.Get("/users", models.GetAllUsers)
+	api.Get("/fuels", models.GetAllFuels)
+	api.Get("/transmissions", models.GetAllTransmissions)
+	api.Get("/colors", models.GetAllColors)
+
 	log.Fatal(app.Listen(":3000"))
 }

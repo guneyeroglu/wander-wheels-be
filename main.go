eb20c706d72eec9ca3f86bcf82f917504fb239c3
guneyeroglu <benguneyeroglu@gmail.com>
2024-04-05 20:12:20 +0300

    #update: new route api and middleware (cors)

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/guneyeroglu/wander-wheels-be/middleware"
 	"github.com/guneyeroglu/wander-wheels-be/models"
 )
 
 func main() {
 	app := fiber.New()
-	app.Use(middleware.Language)
+	app.Use(cors.New(), middleware.Language)
 	api := app.Group("/api")
 
 	api.Get("/users", models.GetAllUsers)
 	api.Get("/fuels", models.GetAllFuels)
 	api.Get("/transmissions", models.GetAllTransmissions)
 	api.Get("/colors", models.GetAllColors)
+	api.Get("/brands", models.GetAllBrands)
+	api.Get("/models", models.GetAllModels)
 
 	log.Fatal(app.Listen(":3000"))
 }

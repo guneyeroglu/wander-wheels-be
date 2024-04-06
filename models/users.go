7e4a3043d622a149867250d1b1958b4bb6b98a18
guneyeroglu <benguneyeroglu@gmail.com>
2024-04-06 15:32:36 +0300

    #update: users changed (added role)

--- models/users.go
+++ models/users.go
@@ -6,10 +6,15 @@ import (
 	"github.com/guneyeroglu/wander-wheels-be/database"
 )
 
+type Role struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
 type User struct {
 	Id       uuid.UUID `json:"id"`
 	Username string    `json:"username"`
 	Password string    `json:"password"`
+	Role     Role      `json:"role"`
 }
 
 func GetAllUsers(c *fiber.Ctx) error {
@@ -20,10 +25,13 @@ func GetAllUsers(c *fiber.Ctx) error {
 
 	rows, err := db.Query(`
 		SELECT 
-			id, 
-			username, 
-			password 
-		FROM users
+			U.id, 
+			U.username, 
+			U.password,
+			R.id,
+			R.name 
+		FROM users AS U
+		JOIN roles AS R ON r.id = u.role_id
 	`)
 
 	if err != nil {
@@ -32,13 +40,15 @@ func GetAllUsers(c *fiber.Ctx) error {
 
 	for rows.Next() {
 		var user User
+		var role Role
 
-		err := rows.Scan(&user.Id, &user.Username, &user.Password)
+		err := rows.Scan(&user.Id, &user.Username, &user.Password, &role.Id, &role.Name)
 
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
 
+		user.Role = role
 		users = append(users, user)
 	}
 

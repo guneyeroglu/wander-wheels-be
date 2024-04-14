0368c8b3298374e4d612b3ad9f82a75a8349e4a9
guneyeroglu <benguneyeroglu@gmail.com>
2024-04-14 15:06:01 +0300

    #update: token added, new api, old api changed for login

--- models/users.go
+++ models/users.go
@@ -1,9 +1,12 @@
 package models
 
 import (
-	"fmt"
+	"os"
+	"strings"
+	"time"
 
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/guneyeroglu/wander-wheels-be/database"
 	"github.com/guneyeroglu/wander-wheels-be/utils"
@@ -23,36 +26,37 @@ type User struct {
 }
 
 type LoginData struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Id       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Role     Role      `json:"role"`
 }
 
-func Login(c fiber.Ctx) error {
+func Login(c *fiber.Ctx) error {
 	lang := c.Locals("lang").(string)
 	db := database.ConnectDb()
 	defer db.Close()
 
 	var data LoginData
 
-	if err := c.Bind().JSON(&data); err != nil {
+	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
 			"status":  fiber.StatusUnprocessableEntity,
 			"message": err.Error(),
 		})
 	}
 
-	var users []User
+	var userData []LoginData
 
 	query := `
-		SELECT 
-			U.id, 
+		SELECT
+			U.id,
 			U.username,
-			U.mail, 
 			U.password,
 			R.id,
-			R.name 
+			R.name
 		FROM users AS U
-		JOIN roles AS R ON r.id = u.role_id
+		JOIN roles as R ON r.id = u.role_id
 		WHERE 
 			(U.username = $1) AND
 			(U.password = $2)
@@ -69,10 +73,113 @@ func Login(c fiber.Ctx) error {
 	}
 
 	for rows.Next() {
+		var user LoginData
+		var role Role
+
+		err := rows.Scan(&user.Id, &user.Username, &user.Password, &role.Id, &role.Name)
+
+		if err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"status":  fiber.StatusUnprocessableEntity,
+				"message": err.Error(),
+			})
+		}
+
+		user.Role = role
+		userData = append(userData, user)
+	}
+
+	if userData == nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  fiber.StatusNotFound,
+			"data":    nil,
+			"message": utils.GetTranslation(lang, "notFound"),
+		})
+	}
+
+	claims := jwt.MapClaims{
+		"id":       userData[0].Id,
+		"username": userData[0].Username,
+		"roleName": userData[0].Role.Name,
+		"exp":      time.Now().Add(time.Hour * 24 * 1).Unix(), // 1 day
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	jwtSecretCode := os.Getenv("JWT_SECRET_CODE")
+
+	t, err := token.SignedString([]byte(jwtSecretCode))
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  fiber.StatusUnprocessableEntity,
+			"message": err.Error(),
+		})
+	}
+
+	loginResponse := map[string]interface{}{
+		"username": userData[0].Username,
+		"token":    t,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  fiber.StatusOK,
+		"data":    loginResponse,
+		"message": utils.GetTranslation(lang, "success"),
+	})
+}
+
+func GetUserInfo(c *fiber.Ctx) error {
+	lang := c.Locals("lang").(string)
+	db := database.ConnectDb()
+	defer db.Close()
+
+	tokenType := os.Getenv("JWT_TOKEN_TYPE")
+	jwtSecretCode := os.Getenv("JWT_SECRET_CODE")
+	tokenString := strings.Join(strings.Split(c.Get("Authorization"), tokenType), "")
+	claims := jwt.MapClaims{}
+
+	parseToken, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecretCode), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  fiber.StatusUnprocessableEntity,
+			"message": err.Error(),
+		})
+	}
+
+	id := parseToken.Claims.(jwt.MapClaims)["id"].(string)
+
+	var users []User
+
+	query := `
+		SELECT 
+			U.id, 
+			U.username,
+			U.mail, 
+			R.id,
+			R.name 
+		FROM users AS U
+		JOIN roles AS R ON r.id = u.role_id
+		WHERE 
+			(U.id = $1)
+		ORDER BY U.username ASC
+	`
+
+	rows, err := db.Query(query, id)
+
+	if err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  fiber.StatusUnprocessableEntity,
+			"message": err.Error(),
+		})
+	}
+
+	for rows.Next() {
 		var user User
 		var role Role
 
-		err := rows.Scan(&user.Id, &user.Username, &user.Mail, &user.Password, &role.Id, &role.Name)
+		err := rows.Scan(&user.Id, &user.Username, &user.Mail, &role.Id, &role.Name)
 
 		if err != nil {
 			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
@@ -93,7 +200,8 @@ func Login(c fiber.Ctx) error {
 		})
 	}
 
-	userInfo := map[string]interface{}{
+	userInfo := jwt.MapClaims{
+		"id":       users[0].Id,
 		"username": users[0].Username,
 		"mail":     users[0].Mail,
 		"role":     users[0].Role,
@@ -112,7 +220,7 @@ type SignUpData struct {
 	Password *string `json:"password"`
 }
 
-func SignUp(c fiber.Ctx) error {
+func SignUp(c *fiber.Ctx) error {
 	lang := c.Locals("lang").(string)
 	roleId := 2 // customer
 	db := database.ConnectDb()
@@ -136,7 +244,7 @@ func SignUp(c fiber.Ctx) error {
 
 	var data SignUpData
 
-	if err := c.Bind().JSON(&data); err != nil {
+	if err := c.BodyParser(&data); err != nil {
 		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
 			"status":  fiber.StatusUnprocessableEntity,
 			"message": err.Error(),
@@ -170,8 +278,6 @@ func SignUp(c fiber.Ctx) error {
 		user.Role = role
 		users = append(users, user)
 	}
-
-	fmt.Println(users)
 
 	if users != nil {
 		return c.Status(fiber.StatusConflict).JSON(fiber.Map{

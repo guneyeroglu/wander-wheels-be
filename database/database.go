9b128d4bf3cf32cee6bb8d24335b51c9ebe03146
guneyeroglu <benguneyeroglu@gmail.com>
2024-05-04 20:56:24 +0300

    #update: string changed for railway -fn

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -17,15 +16,16 @@ func ConnectDb() *sql.DB {
 		log.Fatal("Error loading .env file")
 	}
 
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbConnectionUrl := os.Getenv("DB_CONNECTION_URL")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbSslMode := os.Getenv("DB_SSL_MODE")
+	// dbUsername := os.Getenv("DB_USERNAME")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+	// dbConnectionUrl := os.Getenv("DB_CONNECTION_URL")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_NAME")
+	// dbSslMode := os.Getenv("DB_SSL_MODE")
+	dbConnection := os.Getenv("DB_CONNECTION")
 
-	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", dbUsername, dbPassword, dbConnectionUrl, dbPort, dbName, dbSslMode)
-	db, err := sql.Open("postgres", connectionString)
+	// connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", dbUsername, dbPassword, dbConnectionUrl, dbPort, dbName, dbSslMode)
+	db, err := sql.Open("postgres", dbConnection)
 
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)

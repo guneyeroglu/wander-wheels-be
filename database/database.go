cd993b06abb269be3d0b0b6844ad6ccddf7dc9f9
guneyeroglu <benguneyeroglu@gmail.com>
2024-05-04 23:59:22 +0300

    #update: viper added for env

--- database/database.go
+++ database/database.go
@@ -4,17 +4,27 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func ConnectDb() *sql.DB {
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbConnectionUrl := os.Getenv("DB_CONNECTION_URL")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	viper.AddConfigPath(".")
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatal("Error", err)
+
+	}
+
+	dbUsername := viper.Get("DB_USERNAME").(string)
+	dbPassword := viper.Get("DB_PASSWORD").(string)
+	dbConnectionUrl := viper.Get("DB_CONNECTION_URL").(string)
+	dbPort := viper.Get("DB_PORT").(string)
+	dbName := viper.Get("DB_NAME").(string)
 
 	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", dbUsername, dbPassword, dbConnectionUrl, dbPort, dbName)
 	db, err := sql.Open("postgres", connectionString)
